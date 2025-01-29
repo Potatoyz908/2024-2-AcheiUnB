4b93e5dc144ab66c4b2233b5d789be23e974680e
Potatoyz908 <euller2005@gmail.com>
2025-01-29 11:59:05 -0300

    chore: adiciona caminho para tarefas do aplicativo 'users' na configuração do Celery

--- API/AcheiUnB/celery.py
+++ API/AcheiUnB/celery.py
@@ -3,7 +3,8 @@ import os
 from celery import Celery
 
 # Define a configuração do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AcheiUnB.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AcheiUnB.settings",
+                      "API.users.tasks")
 
 app = Celery("AcheiUnB")
 
