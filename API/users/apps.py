1a416f70220fbdf505671f260fa16ccac7eb1e5a
Potatoyz908 <euller2005@gmail.com>
2025-01-29 12:45:28 -0300

    chore: ajusta a importação de sinais no aplicativo 'users' para usar o caminho completo 'API.users.signals'

--- API/users/apps.py
+++ API/users/apps.py
@@ -1,10 +1,8 @@
 from django.apps import AppConfig
 
-
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "API.users"
 
     def ready(self):
-        # Importe os sinais dentro do método ready
-        import users.signals  # Certifique-se de que o arquivo signals.py existe e está configurado
+        import API.users.signals  # Ajuste também para 'API.users.signals' se quiser ser consistente
