5aed59cb5f4a7e0e1e45f1a5508a1c3c30fbe488
Potatoyz908 <euller2005@gmail.com>
2025-01-29 12:19:21 -0300

    chore: remove caminho desnecessário do módulo de configurações do Django no arquivo Celery

--- API/AcheiUnB/celery.py
+++ API/AcheiUnB/celery.py
@@ -3,8 +3,7 @@ import os
 from celery import Celery
 
 # Define a configuração do Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.AcheiUnB.settings",
-                      "API.users.tasks")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.AcheiUnB.settings")
 
 app = Celery("AcheiUnB")
 
