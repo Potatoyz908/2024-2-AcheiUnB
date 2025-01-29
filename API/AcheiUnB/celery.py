7897486368fe0d62cca67905cd7f9a94ef025959
Potatoyz908 <euller2005@gmail.com>
2025-01-28 22:35:13 -0300

    chore: atualiza caminho do módulo de configurações do Django no celery.py

--- API/AcheiUnB/celery.py
+++ API/AcheiUnB/celery.py
@@ -2,16 +2,15 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Define o módulo de configurações padrão do Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AcheiUnB.settings')
+# Configuração do Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'API.AcheiUnB.settings')
 
-# Instancia o Celery
 app = Celery('AcheiUnB')
 
-# Carrega as configurações do Django no Celery
+# Carrega configurações do Celery do Django
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Descobre automaticamente as tasks registradas em INSTALLED_APPS
+# Descobre tasks automaticamente
 app.autodiscover_tasks()
 
 @app.task(bind=True)
