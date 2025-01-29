5666ff42416b0451c0ad9072c0a2897321b0d8ec
Potatoyz908 <euller2005@gmail.com>
2025-01-29 11:41:16 -0300

    chore: corrige configuração do Django e nome do aplicativo no módulo Celery

--- API/AcheiUnB/celery.py
+++ API/AcheiUnB/celery.py
@@ -2,17 +2,17 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# Configuração do Django
-os.environ('DJANGO_SETTINGS_MODULE')
+# Define a configuração do Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "API.AcheiUnB.settings")
 
-app = Celery('AcheiUnb')
+app = Celery("AcheiUnB")
 
-# Carrega configurações do Celery do Django
-app.config_from_object('django.conf:settings', namespace='CELERY')
+# Carrega as configurações do Django
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Descobre tasks automaticamente
+# Descobre tasks automaticamente nos apps instalados
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f"Request: {self.request!r}")
