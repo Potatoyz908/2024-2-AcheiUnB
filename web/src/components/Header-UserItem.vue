06da92394d76814a653fb2da0ab33cc3da1db220
314dro <silvampedro001@gmail.com>
2025-06-16 21:26:07 -0300

    fix(header): Centralizando título do header no cadastro de itens em telas grandes e corrigindo espaçamento vertical da logo no header

--- web/src/components/Header-UserItem.vue
+++ web/src/components/Header-UserItem.vue
@@ -2,25 +2,21 @@
   <div
     class="w-full h-[100px] bg-verde shadow-md rounded-b-xl grid grid-cols-[1fr_2fr_1fr] items-center px-4 text-white"
   >
-    <!-- Coluna da esquerda: botão voltar -->
     <router-link to="/user" class="flex justify-start">
       <img
         src="@/assets/icons/arrow-left-white.svg"
         alt="Voltar"
-        class="w-[30px] h-[30px] hover:scale-125 transition-transform"
+        class="w-[35px] h-[35px] hover:scale-125 transition-transform"
       />
     </router-link>
 
-    <!-- Coluna central: título -->
     <div class="text-center">
       <h1 class="text-2xl sm:text-2xl md:text-3xl font-bold">Meus Itens</h1>
     </div>
 
-    <!-- Coluna da direita: logo -->
-    <router-link to="/about" class="flex justify-end pr-0 md:pr-4">
-    <Logo sizeClass="text-xl sm:text-2xl md:text-2xl" />
+    <router-link to="/about" class="flex justify-end items-center md:pr-4">
+      <Logo class="transform translate-y-[1.25px]" sizeClass="text-xl sm:text-2xl md:text-2xl" />
     </router-link>
-
   </div>
 </template>
 
